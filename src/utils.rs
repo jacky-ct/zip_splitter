a8a818f26967351a370db78a1d822ff8952b3ac0
Jackson Thomas <jacksonchristopherthomas@gmail.com>
2024-11-26 22:25:16 +0000

    impl print_and_get_input

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::error::Error as Error;
 use std::ffi::OsStr;
+use std::io::Write;
+use std::io;
 
 pub fn zip_in_dir(dir: &str) -> Result<bool, Box<dyn Error>> {
     Ok(fs::read_dir(dir)?
@@ -13,3 +15,11 @@ pub fn zip_in_dir(dir: &str) -> Result<bool, Box<dyn Error>> {
         .any(|x| x))
 }
 
+pub fn print_and_get_input(msg: &str) -> String {
+    print!("{}", msg);
+    let mut input = String::new();
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    input
+}
