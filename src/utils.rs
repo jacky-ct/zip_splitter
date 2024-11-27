bfe56d18e416abd3a058e2d1e72711e61e2946f7
Jackson Thomas <jacksonchristopherthomas@gmail.com>
2024-11-27 11:40:52 +0000

    impl get_file_sizes_in_dir

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,7 @@ use std::error::Error as Error;
 use std::ffi::OsStr;
 use std::io::Write;
 use std::io;
+use filesize::PathExt;
 
 pub fn zip_in_dir(dir: &str) -> Result<bool, Box<dyn Error>> {
     Ok(fs::read_dir(dir)?
@@ -22,4 +23,14 @@ pub fn print_and_get_input(msg: &str) -> String {
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
     input
+}
+
+pub fn get_file_sizes_in_dir(dir: &str) -> Result<Vec<(u64, String)>, Box<dyn Error>> {
+    Ok(
+        fs::read_dir(dir)?
+        .filter_map(|res| res.ok())
+        .map(|dir| dir.path())
+        .map(|path| (path.size_on_disk().expect("Error reading file size"), String::from(path.to_str().unwrap())))
+        .collect()
+    )
 }
