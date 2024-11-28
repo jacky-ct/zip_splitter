838e25de80d20db6d016694350fcaee1580263a3
Jackson Thomas <jacksonchristopherthomas@gmail.com>
2024-11-28 16:48:15 +0000

    impl get_zip_in_dir to replace bool variant

--- src/utils.rs
+++ src/utils.rs
@@ -5,6 +5,7 @@ use std::io::Write;
 use std::io;
 use filesize::PathExt;
 use std::str::FromStr as from_str;
+use std::path::PathBuf;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug)]
 pub struct File {
@@ -14,14 +15,27 @@ pub struct File {
 }
 
 pub fn zip_in_dir(dir: &str) -> Result<bool, Box<dyn Error>> {
-    Ok(fs::read_dir(dir)?
+    let has_zip = fs::read_dir(dir)?
         .filter_map(|res| res.ok())
         .map(|dir| dir.path())
         .map(|path| path.extension()
             .unwrap_or(OsStr::new(""))
             .to_str()
             .unwrap_or("") == "zip")
-        .any(|x| x))
+        .any(|x| x);
+
+    Ok(has_zip)
+}
+
+pub fn get_zip_in_dir(dir: &str) -> Option<PathBuf> {
+    let mut dirs = fs::read_dir(dir).ok()?
+        .filter_map(|res| res.ok())
+        .map(|dir| dir.path());
+
+    dirs.find(|x| x.extension()
+        .unwrap_or(OsStr::new(""))
+        .to_str()
+        .unwrap_or("") == "zip")
 }
 
 pub fn print_and_get_input(msg: &str) -> String {
@@ -47,9 +61,10 @@ pub fn get_file_sizes_in_dir(dir: &str) -> Result<Vec<File>, Box<dyn Error>> {
 }
 
 pub fn max_file_size_from_input(input: &str) -> Result<u64, Box<dyn Error>> {
-    if input == "" {return Ok(10_737_418_240)}
+    if input.trim().is_empty() {return Ok(10_737_418_240)}
 
     let len = input.chars().count();
+    if len < 3 {return Err("Invalid input".into())};
 
     let size = u64::from_str(&input[..len-3])?;
     let unit = &input[len-3..];
